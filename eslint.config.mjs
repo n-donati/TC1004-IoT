35f127f8903ad00a45acb08d4030fe62d8cd6bc5
n-donati <nico.donati@pm.me>
2024-10-08 17:05:24 -0600

    [FETCH] eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,10 @@ import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  { ignores: ["dist/"]},
-  {files: ["src/**/*.{js,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { ignores: ["dist/"] },
+  { files: ["src/**/*.{js,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
